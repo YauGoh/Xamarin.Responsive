c0d7d2b315ec93ad6e1a668f45fa2788a92a70e0
Yau Goh Chow <yau_@hotmail.com>
2021-01-23 13:09:06 +0800

    Add support for Xamarin.Forms from > 3.5

--- src/Xamarin.Responsive.Samples/Xamarin.Responsive.Samples.Android/MainActivity.cs
+++ src/Xamarin.Responsive.Samples/Xamarin.Responsive.Samples.Android/MainActivity.cs
@@ -21,14 +21,11 @@ namespace Xamarin.Responsive.Samples.Droid
 
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
